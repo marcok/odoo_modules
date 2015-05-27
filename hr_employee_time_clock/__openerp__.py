0700953298580838416b8e28713162151b29aeaf
kundi <kundert.marco@gmail.com>
2015-05-27 10:48:44 +0200

    added dependency, changed description

--- hr_employee_time_clock/__openerp__.py
+++ hr_employee_time_clock/__openerp__.py
@@ -5,7 +5,7 @@
     'website': "http://www.bytebrand.net",
     'category': 'Human Resources',
     'version': '1.0',
-    'depends': ['hr_timesheet_sheet', 'hr_attendance', 'hr_contract'],
+    'depends': ['hr_timesheet_sheet', 'hr_attendance', 'hr_contract', 'hr_attendance_analysis'],
 	'images': ['images/overundertime.png'],
 	'installable': True,
     'data': [
