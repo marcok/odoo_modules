33b70bafc69e3192089888ba9eff6d324204f2b2
additionsolutions <odoo@aitspl.com>
2015-08-27 14:49:32 +0530

    Fixed problem of 0.5h in duty hours computation.

--- hr_employee_time_clock/models.py
+++ hr_employee_time_clock/models.py
@@ -1,12 +1,71 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
+import math
 
 from openerp.osv import fields, osv
 from dateutil import rrule, parser
 import pytz
 from openerp.tools.translate import _
 
+# Done BY Addition IT Solutions: BEGIN
+class time_clock_resource_calendar(osv.osv):
+    _inherit = "resource.calendar"
+
+    def get_working_intervals_of_day(self, cr, uid, id, start_dt=None, end_dt=None,
+                                     leaves=None, compute_leaves=False, resource_id=None,
+                                     default_interval=None, context=None):
+        """ To resolve issue of 0.5h on duty hours, this method has to be overriden here.
+        """
+        if isinstance(id, (list, tuple)):
+            id = id[0]
+
+        # Computes start_dt, end_dt (with default values if not set) + off-interval work limits
+        work_limits = []
+        if start_dt is None and end_dt is not None:
+            start_dt = end_dt.replace(hour=0, minute=0, second=0)
+        elif start_dt is None:
+            start_dt = datetime.datetime.now().replace(hour=0, minute=0, second=0)
+        else:
+            work_limits.append((start_dt.replace(hour=0, minute=0, second=0), start_dt))
+        if end_dt is None:
+            end_dt = start_dt.replace(hour=23, minute=59, second=59)
+        else:
+            work_limits.append((end_dt, end_dt.replace(hour=23, minute=59, second=59)))
+        assert start_dt.date() == end_dt.date(), 'get_working_intervals_of_day is restricted to one day'
+
+        intervals = []
+        work_dt = start_dt.replace(hour=0, minute=0, second=0)
+
+        # no calendar: try to use the default_interval, then return directly
+        if id is None:
+            if default_interval:
+                working_interval = (start_dt.replace(hour=default_interval[0], minute=0, second=0), start_dt.replace(hour=default_interval[1], minute=0, second=0))
+            intervals = self.interval_remove_leaves(working_interval, work_limits)
+            return intervals
+
+        working_intervals = []
+        for calendar_working_day in self.get_attendances_for_weekdays(cr, uid, id, [start_dt.weekday()], context):
+            # FIXED by Addition IT Solutions: Counting minutes to get result when 0.5h are added to calendar 
+            minutes_from = math.modf(calendar_working_day.hour_from)[0] * 60
+            minutes_to = math.modf(calendar_working_day.hour_to)[0] * 60
+            working_interval = (
+                work_dt.replace(hour=int(calendar_working_day.hour_from),minute=int(minutes_from)),
+                work_dt.replace(hour=int(calendar_working_day.hour_to),minute=int(minutes_to))
+            )
+            working_intervals += self.interval_remove_leaves(working_interval, work_limits)
+
+        # find leave intervals
+        if leaves is None and compute_leaves:
+            leaves = self.get_leave_intervals(cr, uid, id, resource_id=resource_id, context=None)
+
+        # filter according to leaves
+        for interval in working_intervals:
+            work_intervals = self.interval_remove_leaves(interval, leaves)
+            intervals += work_intervals
+
+        return intervals
+# END
 
 class hr_timesheet_dh(osv.osv):
     """
