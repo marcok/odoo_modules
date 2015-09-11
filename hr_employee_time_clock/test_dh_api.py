38affac9f09e11e25c12c2e2bd05d6dca398a3a3
marco <marco@lemberg.co.uk>
2015-09-11 12:09:59 +0300

    remove login

--- hr_employee_time_clock/test_dh_api.py
+++ hr_employee_time_clock/test_dh_api.py
@@ -5,13 +5,9 @@ import datetime
 from dateutil import parser
 
 #url = "http://localhost:8069"
-url = "http://217.20.187.163:8069"
 #db = "stclaus_1"
-db = "Hosberg"
 #username = "admin"
 #password = 'admin'
-username = "iphone@qarea.com"
-password = "iphone"
 
 common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
 print common.version()
