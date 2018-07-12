429f24afe788bedfc486d15dbb7d5152c97847f7
Anastasiia Churyk <anastasia.churyk@bytebrand.net>
2018-07-12 12:43:24 +0300

    extension [FIX] remove loggers

--- hr_employee_time_clock/models/hr_attendance_analysis.py
+++ hr_employee_time_clock/models/hr_attendance_analysis.py
@@ -81,8 +81,6 @@ class HrAttendance(models.Model):
     @api.multi
     def _get_current_sheet(self, employee_id, date=False):
         sheet_obj = self.env['hr_timesheet_sheet.sheet']
-        logging.getLogger(employee_id)
-        logging.getLogger(date)
         if not date:
             date = time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
 
@@ -124,7 +122,6 @@ class HrAttendance(models.Model):
     def _sheet(self):
         res = {}.fromkeys(self, False)
         for attendance in self:
-            logging.getLogger(attendance.name)
             sheet = self._get_current_sheet(
                 attendance.employee_id.id, attendance.check_in)
             if sheet:
