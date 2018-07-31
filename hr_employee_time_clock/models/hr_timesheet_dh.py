4a83f4b73d932f8b2f8c26f584f395913a282479
Anatolij Yakumets <anatolij.yakymets@bytebrand.net>
2018-07-31 11:10:31 +0300

    fix

--- hr_employee_time_clock/models/hr_timesheet_dh.py
+++ hr_employee_time_clock/models/hr_timesheet_dh.py
@@ -203,50 +203,6 @@ class HrTimesheetDh(models.Model):
             leave_descr = descr[0].name
         return leave_descr
 
-    # def get_date_format(self, values):
-    #     if values[0][0].find('* ') != -1:
-    #         values[0][0] = values[0][0].replace('* ', '')
-    #     splitter = '.'
-    #     for symbol in ['-', '/', '.']:
-    #         find_splitter = values[0][0].find(symbol)
-    #         if find_splitter != -1:
-    #             splitter = symbol
-    #             break
-    #     year = str(datetime.today().year)
-    #     year_index_find = values[0][0].find(year)
-    #     if year_index_find == 6:
-    #         year_index = 2
-    #         find_month_index = [0, 1]
-    #     else:
-    #         year_index = 0
-    #         find_month_index = [1, 2]
-    #     month_index = 0
-    #     for i in range(len(values) - 1):
-    #         if values[i][0].find('* ') != -1:
-    #             values[i][0] = values[i][0].replace('* ', '')
-    #         if values[i + 1][0].find('* ') != -1:
-    #             values[i + 1][0] = values[i + 1][0].replace('* ', '')
-    #         value_lst_1 = values[i][0].split(splitter)
-    #         value_lst_2 = values[i + 1][0].split(splitter)
-    #         if value_lst_1[find_month_index[0]] == value_lst_2[
-    #             find_month_index[0]]:
-    #             month_index = find_month_index[0]
-    #             break
-    #         if value_lst_1[find_month_index[1]] == value_lst_2[
-    #             find_month_index[1]]:
-    #             month_index = find_month_index[1]
-    #             break
-
-        indexes = [0, 1, 2]
-        for number in [month_index, year_index]:
-            indexes.remove(number)
-        day_index = indexes[0]
-        format_unsorted = {day_index: 'd', month_index: 'm', year_index: 'Y'}
-        date_format = '%' + format_unsorted.get(0) + splitter + '%' \
-                      + format_unsorted.get(1) + splitter + '%' \
-                      + format_unsorted.get(2)
-
-        return date_format
 
     @api.multi
     def _get_analysis(self):
@@ -271,7 +227,8 @@ class HrTimesheetDh(models.Model):
                 '{padding: 3px; border: 1px solid #C0C0C0; '
                 'border-collapse: collapse;     '
                 'text-align: right;} '
-                '.attendanceTable {border: 1px solid #C0C0C0;}</style><table class="attendanceTable">']
+                '.attendanceTable {border: 1px solid #C0C0C0;}</style>'
+                '<table class="attendanceTable">']
             for val in data.values():
                 if isinstance(val, (int, float)):
                     t = '{0:02.0f}:{1:02.0f}'.format(
@@ -288,13 +245,14 @@ class HrTimesheetDh(models.Model):
                     else:
                         output.append('<td colspan="4">' + t + '</td>')
                     output.append('</tr>')
+
+            keys = (_('Date'), _('Duty Hours'), _('Worked Hours'),
+                    _('Difference'), _('Running'), _('Leaves'))
             if use_overtime:
                 keys = (_('Date'), _('Duty Hours'), _('Worked Hours'),
                         _('Bonus Hours'), _('Night Shift'),
                         _('Difference'), _('Running'), _('Leaves'))
-            else:
-                keys = (_('Date'), _('Duty Hours'), _('Worked Hours'),
-                        _('Difference'), _('Running'), _('Leaves'))
+
             a = ('previous_month_diff', 'hours', 'total')
             for k in a:
                 v = data.get(k)
@@ -307,15 +265,13 @@ class HrTimesheetDh(models.Model):
                     output.append('</tr>')
                     for res in v:
                         values.append([res.get(key) for key in keys])
-                    # date_format = self.get_date_format(values)
                     date_format, time_format = \
                         self._get_user_datetime_format()
                     for tr in values:
                         formatted_tr = tr[0]
-                        if formatted_tr.find('* ') != -1:
-                            formatted_tr = formatted_tr.replace('* ', '')
-                        sheet_day = datetime.strptime(formatted_tr,
-                                                      date_format).date()
+                        formatted_tr = formatted_tr.replace('* ', '')
+                        sheet_day = datetime.strptime(
+                            formatted_tr, date_format).date()
                         if datetime.today().date() == sheet_day:
                             output.append(
                                 '<tr style="background-color:#bdde9f96;">')
@@ -366,115 +322,6 @@ class HrTimesheetDh(models.Model):
             output.append('</table>')
             sheet['analysis'] = '\n'.join(output)
 
-    # def get_date_format(self, values):
-    #     splitter = '.'
-    #     for symbol in ['-', '/', '.']:
-    #         find_splitter = values[0][0].find(symbol)
-    #         if find_splitter != -1:
-    #             splitter = symbol
-    #             break
-    #     year = str(datetime.today().year)
-    #     year_index_find = values[0][0].find(year)
-    #     if year_index_find == 6:
-    #         year_index = 2
-    #         find_month_index = [0, 1]
-    #     else:
-    #         year_index = 0
-    #         find_month_index = [1, 2]
-    #     month_index = 0
-    #     for i in range(len(values) - 1):
-    #         value_lst_1 = values[i][0].split(splitter)
-    #         value_lst_2 = values[i + 1][0].split(splitter)
-    #         if value_lst_1[find_month_index[0]] == value_lst_2[
-    #                                                     find_month_index[0]]:
-    #             month_index = find_month_index[0]
-    #             break
-    #         if value_lst_1[find_month_index[1]] == value_lst_2[
-    #                                                     find_month_index[1]]:
-    #             month_index = find_month_index[1]
-    #             break
-    #
-    #     indexes = [0, 1, 2]
-    #     for number in [month_index, year_index]:
-    #         indexes.remove(number)
-    #     day_index = indexes[0]
-    #     format_unsorted = {day_index: 'd', month_index: 'm', year_index: 'Y'}
-    #     date_format = '%' + format_unsorted.get(0) + splitter + '%'\
-    #                   + format_unsorted.get(1) + splitter + '%' \
-    #                   + format_unsorted.get(2)
-    #
-    #     return date_format
-
-    # @api.multi
-    # def _get_analysis(self):
-    #     for sheet in self:
-    #         function_call = True
-    #         ctx = self.env.context.copy()
-    #         ctx['online_analysis'] = True
-    #         data = self.with_context(ctx).attendance_analysis(
-    #             timesheet_id=sheet.id, function_call=function_call)
-    #         values = []
-    #         output = [
-    #             '<style>.attendanceTable td,.attendanceTable th '
-    #             '{padding: 3px; border: 1px solid #C0C0C0; '
-    #             'border-collapse: collapse;     '
-    #             'text-align: right;} </style><table class="attendanceTable" >']
-    #         for val in data.values():
-    #             if isinstance(val, (int, float)):
-    #                 t = '{0:02.0f}:{1:02.0f}'.format(
-    #                     *divmod(float(val) * 60, 60))
-    #                 if val < 0:
-    #                     t = '-{0:02.0f}:{1:02.0f}'.format(
-    #                         *divmod(float(val) * -60, 60))
-    #
-    #                 output.append('<tr>')
-    #                 prev_ts = _('Previous Attendance Sheet:')
-    #                 output.append('<th colspan="2">' + prev_ts + ' </th>')
-    #                 output.append('<td colspan="3">' + t + '</td>')
-    #                 output.append('</tr>')
-    #         keys = (_('Date'), _('Duty Hours'), _('Worked Hours'),
-    #                 _('Difference'), _('Running'))
-    #         a = ('previous_month_diff', 'hours', 'total')
-    #         for k in a:
-    #             v = data.get(k)
-    #             if isinstance(v, list):
-    #                 output.append('<tr>')
-    #
-    #                 for th in keys:
-    #                     output.append('<th>' + th + '</th>')
-    #                 output.append('</tr>')
-    #                 for res in v:
-    #                     values.append([res.get(key) for key in keys])
-    #                 date_format = self.get_date_format(values)
-    #                 for tr in values:
-    #                     sheet_day = datetime.strptime(tr[0], date_format).date()
-    #                     if datetime.today().date() == sheet_day:
-    #                         output.append('<tr style="background-color:#bdde9f96;">')
-    #                     else:
-    #                         output.append('<tr>')
-    #                     for td in tr:
-    #                         if not td:
-    #                             td = '-'
-    #                         output.append('<td>' + td + '</td>')
-    #                     output.append('</tr>')
-    #
-    #             if isinstance(v, dict):
-    #                 output.append('<tr>')
-    #                 total_ts = _('Total:')
-    #                 output.append('<th>' + total_ts + ' </th>')
-    #                 for td in ('duty_hours', 'worked_hours',
-    #                            'work_current_month_diff', 'diff'):
-    #                     t = '{0:02.0f}:{1:02.0f}'.format(
-    #                         *divmod(float(round(v.get(td), 4)) * 60, 60))
-    #                     if float(v.get(td)) < 0.0:
-    #                         t = '-{0:02.0f}:{1:02.0f}'.format(
-    #                             *divmod(float(round(v.get(td), 4)) * -60, 60))
-    #
-    #                     output.append(
-    #                         '<td>' + '%s' % t + '</td>')
-    #                 output.append('</tr>')
-    #         output.append('</table>')
-    #         sheet['analysis'] = '\n'.join(output)
 
     total_duty_hours = fields.Float(compute='_duty_hours',
                                     string='Total Duty Hours',
@@ -581,109 +428,6 @@ class HrTimesheetDh(models.Model):
             date_format = lang.date_format
             time_format = lang.time_format
         return date_format, time_format
-
-    # @api.multi
-    # def attendance_analysis(self, timesheet_id=None, function_call=False):
-    #     attendance_obj = self.env['hr.attendance']
-    #     date_format, time_format = self._get_user_datetime_format()
-    #     if not self.env.context.get('online_analysis') \
-    #             and date_format != '%m/%d/%Y':
-    #         date_format = '%m/%d/%Y'
-    #     for sheet in self.sudo():
-    #         if not timesheet_id:
-    #             timesheet_id = self[-1].id
-    #         if sheet.id == timesheet_id:
-    #             employee_id = sheet.employee_id.id
-    #             start_date = sheet.date_from
-    #             end_date = sheet.date_to
-    #             previous_month_diff = self.get_previous_month_diff(
-    #                 employee_id, start_date)
-    #             current_month_diff = previous_month_diff
-    #             res = {
-    #                 'previous_month_diff': previous_month_diff,
-    #                 'hours': []
-    #             }
-    #
-    #             period = {'date_from': start_date,
-    #                       'date_to': end_date
-    #                       }
-    #             dates = list(rrule.rrule(rrule.DAILY,
-    #                                      dtstart=parser.parse(start_date),
-    #                                      until=parser.parse(end_date)))
-    #             work_current_month_diff = 0.0
-    #             total = {'worked_hours': 0.0, 'duty_hours': 0.0,
-    #                      'diff':
-    #                          current_month_diff, 'work_current_month_diff': ''}
-    #             last_date = dates[-1]
-    #             today_worked_hours = 0.0
-    #             today_diff = 0
-    #             today_current_month_diff = 0
-    #             for date_line in dates:
-    #                 dh = sheet.calculate_duty_hours(date_from=date_line,
-    #                                                 period=period)
-    #                 worked_hours = 0.0
-    #
-    #                 for att in sheet.attendances_ids:
-    #                     user_tz = pytz.timezone(
-    #                         att.employee_id.user_id.tz or 'UTC')
-    #                     att_name = fields.Datetime.from_string(
-    #                         att.name).replace(
-    #                         tzinfo=pytz.utc).astimezone(user_tz)
-    #                     name = att_name.replace(tzinfo=None)
-    #                     if name.strftime('%Y-%m-%d') == \
-    #                             date_line.strftime('%Y-%m-%d'):
-    #                         worked_hours += att.worked_hours
-    #                         if not att.check_out:
-    #                             tz = pytz.timezone('UTC')
-    #                             t = datetime.now(tz=tz)
-    #                             d = t - att_name
-    #                             worked_hours += (d.total_seconds() / 3600)
-    #                 diff = worked_hours - dh
-    #                 current_month_diff += diff
-    #                 work_current_month_diff += diff
-    #                 if date_line.date() == date.today():
-    #                     today_worked_hours = worked_hours
-    #                     today_diff = diff
-    #                     today_current_month_diff =current_month_diff
-    #                 if date_line == last_date:
-    #                     if not self.env.context.get('online_analysis'):
-    #                         worked_hours = today_worked_hours
-    #                         diff = today_diff
-    #                         current_month_diff = work_current_month_diff
-    #                 if date_line == last_date:
-    #                     if not self.env.context.get('online_analysis'):
-    #                         worked_hours = today_worked_hours
-    #                         diff = today_diff
-    #                         current_month_diff = today_current_month_diff
-    #                 if function_call:
-    #                     res['hours'].append({
-    #                         _('Date'): date_line.strftime(date_format),
-    #                         _('Duty Hours'):
-    #                             attendance_obj.float_time_convert(dh),
-    #                         _('Worked Hours'):
-    #                             attendance_obj.float_time_convert(worked_hours),
-    #                         _('Difference'): self.sign_float_time_convert(diff),
-    #                         _('Running'): self.sign_float_time_convert(
-    #                             current_month_diff)})
-    #                 else:
-    #                     res['hours'].append({
-    #                         'name': date_line.strftime(date_format),
-    #                         'dh': attendance_obj.float_time_convert(dh),
-    #                         'worked_hours': attendance_obj.float_time_convert(
-    #                             worked_hours),
-    #                         'diff': self.sign_float_time_convert(diff),
-    #                         'running': self.sign_float_time_convert(
-    #                             current_month_diff)
-    #                     })
-    #                 total['duty_hours'] += dh
-    #                 total['worked_hours'] += worked_hours
-    #                 total['diff'] += diff
-    #                 total['work_current_month_diff'] = work_current_month_diff
-    #
-    #                 res['total'] = total
-    #
-    #
-    #             return res
 
     @api.multi
     def attendance_analysis(self, timesheet_id=None, function_call=False):
