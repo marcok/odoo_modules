92e875d1d5d6730ec42b09ec7bddf90d0310e645
Anastasiia Churyk <anastasia.churyk@bytebrand.net>
2018-08-29 18:10:05 +0300

    dev_11 [FIX] running time calculation fix

--- hr_employee_time_clock/__manifest__.py
+++ hr_employee_time_clock/__manifest__.py
@@ -26,7 +26,7 @@
     'summary': 'Track over- and under-time, generate timesheets, upload public holidays',
     'website': "http://www.bytebrand.net",
     'category': 'Human Resources',
-    'version': '11.0.0.0.6',
+    'version': '11.0.0.0.7',
     'depends': ['mail',
                 'analytic',
                 'hr_timesheet',
