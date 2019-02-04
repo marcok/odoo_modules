dd3b407cbcfc60f6fd31304d8050fe7127d64eab
Anatoliy Yakymets <anatoliy.yakymets@gmail.com>
2019-02-04 15:30:55 +0200

    fix to new apps ver

--- hr_employee_time_clock/__openerp__.py
+++ hr_employee_time_clock/__openerp__.py
@@ -27,7 +27,11 @@
     'website': "http://www.bytebrand.net",
     'category': 'Human Resources',
     'version': '1.2',
-    'depends': ['hr_timesheet_sheet', 'hr_attendance', 'hr_contract', 'hr_holidays'], #,'hr_attendance_analysis'
+    'depends': ['hr_timesheet_sheet',
+                'hr_attendance',
+                'hr_contract',
+                'hr_holidays'],
+    # ,'hr_attendance_analysis'
     'images': ['images/overundertime.png'],
     'installable': True,
     'data': [
