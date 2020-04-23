8ee9c73d3bcfe0e0a938ea8ba76e4d8621924ce9
soulman72 <adler72@gmx.net>
2020-04-23 12:32:09 +0200

    Small bugfix for compute-function
    The original function requires parameters which are not supported by compute of the model.

--- hr_employee_time_clock/models/hr_timesheet_sheet.py
+++ hr_employee_time_clock/models/hr_timesheet_sheet.py
@@ -28,6 +28,33 @@ from odoo.exceptions import ValidationError
 class HrTimesheetSheet(models.Model):
     _inherit = "hr_timesheet_sheet.sheet"
 
+    def _total(self):
+        """
+            Compute the attendances, analytic lines timesheets and differences between them
+            for all the days of a timesheet and the current day.
+        :return:
+        """
+        cr = self.env.cr
+        res = dict.fromkeys(self.ids, {
+            'total_attendance': 0.0,
+            'total_timesheet': 0.0,
+            'total_difference': 0.0,
+        })
+
+        cr.execute("""
+                    SELECT sheet_id as id,
+                           sum(total_attendance) as total_attendance,
+                           sum(total_timesheet) as total_timesheet,
+                           sum(total_difference) as  total_difference
+                    FROM hr_timesheet_sheet_sheet_day
+                    WHERE sheet_id IN %s
+                    GROUP BY sheet_id
+                """, (tuple(self.ids),))
+
+        res.update(dict((x.pop('id'), x) for x in cr.dictfetchall()))
+
+        return res
+    
     def _total(self, cr, uid, ids, name, args, context=None):
         """ Compute the attendances, analytic lines timesheets
         and differences between them
